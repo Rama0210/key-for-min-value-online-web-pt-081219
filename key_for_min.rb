559b8381fa0573cb268ea1152e97895459ed5d97
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-29 00:07:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,4 +7,10 @@ min_value = nil
   name_hash.each do |key, value|
     min_key = key if min_key == nil 
     min_value = value if min_value == nil
+    if value < min_value
+    	min_value = value
+    	min_key = key
+    end
+  end
+  min_key
 end
